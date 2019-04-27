54864d4c71f6c27cebcd4f732043473c288fcd24
itpropro <jdamaschke@outlook.de>
2019-04-27 13:02:33 +0200

    Update run.ps1

--- Azure_Functions/TriggerCapture/run.ps1
+++ Azure_Functions/TriggerCapture/run.ps1
@@ -1,28 +1,65 @@
 using namespace System.Net
 
 # Input bindings are passed in via param block.
-param($Request, $TriggerMetadata)
+param($req,$TriggerMetadata)
 
-# Write to the Azure Functions log stream.
-Write-Host "PowerShell HTTP trigger function processed a request."
+# Acquire API token
+$apiVersion = "2017-09-01"
+$resourceURI = "https://management.azure.com/"
+$tokenAuthURI = $env:MSI_ENDPOINT + "?resource=$resourceURI&api-version=$apiVersion"
+$tokenResponse = Invoke-RestMethod -Method Get -Headers @{"Secret"="$env:MSI_SECRET"} -Uri $tokenAuthURI
+$accessToken = $tokenResponse.access_token
+
+Write-Host $accessToken
+
+$subId = "82cc142a-2850-45ec-8f60-64240c0b804e"
+$vmRg = "serverless_workshop_gab2019"
 
 # Interact with query parameters or the body of the request.
-$name = $Request.Query.Name
-if (-not $name) {
-    $name = $Request.Body.Name
-}
+# $name = $Request.Query.Name
 
-if ($name) {
+$vmName = $req.Body.vmName
+$duration = $req.body.duration
+$capName = "$([Datetime]::utcnow.tostring('MM-dd-hh-mm-ss')).cap"
+
+if ($vmName) {
     $status = [HttpStatusCode]::OK
-    $body = "Hello $name"
 }
 else {
     $status = [HttpStatusCode]::BadRequest
     $body = "Please pass a name on the query string or in the request body."
 }
 
+# Create packet capture
+
+$body = @"
+{
+  "properties": {
+    "target": "/subscriptions/$subId/resourceGroups/$vmRg/providers/Microsoft.Compute/virtualMachines/$vmName",
+    "bytesToCapturePerPacket": 100000,
+    "totalBytesPerSession": 100000,
+    "timeLimitInSeconds": $duration,
+    "storageLocation": {
+      "storageId": "/subscriptions/82cc142a-2850-45ec-8f60-64240c0b804e/resourceGroups/serverless_workshop_gab2019/providers/Microsoft.Storage/storageAccounts/gab2019captures",
+      "storagePath": "https://gab2019captures.blob.core.windows.net/captures/$capName"
+    }
+  }
+}
+"@
+
+$requestArguments = @{ 
+    Uri = "https://management.azure.com/subscriptions/82cc142a-2850-45ec-8f60-64240c0b804e/resourceGroups/NetworkWatcherRG/providers/Microsoft.Network/networkWatchers/NetworkWatcher_westeurope/packetCaptures/$($capName)?api-version=2018-11-01"
+    Method = 'PUT'
+    Headers = @{Authorization="Bearer $accessToken"}
+    contentType = 'application/json'
+    Body = $body
+}
+
+$response = Invoke-WebRequest @requestArguments
+$parsed = $response | convertfrom-json
+
 # Associate values to output bindings by calling 'Push-OutputBinding'.
 Push-OutputBinding -Name Response -Value ([HttpResponseContext]@{
     StatusCode = $status
-    Body = $body
+    Body = $parsed.properties.storageLocation.storagePath
 })
